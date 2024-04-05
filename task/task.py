bb2081374f96c3bbfba58d07f275015f21f31c84
vdanylo <danil.voronov@knu.ua>
2024-04-05 13:06:00 +0300

    added write_filter_result in task.py

--- task/task.py
+++ task/task.py
@@ -10,3 +10,16 @@ def get_input_lines(input_file: str) -> [str]:
     with open(input_file, 'r') as file:
         lines = file.readlines()
     return lines
+
+def write_filter_result(output_file: str, filtered_lines: [str]) -> None:
+    """Write filtered lines to an output file.
+
+    Args:
+        output_file (str): The path to the output file.
+        filtered_lines (list): A list of filtered lines to be written to the output file.
+
+    Returns:
+        None
+    """
+    with open(output_file, 'w') as file:
+        file.writelines(filtered_lines)
